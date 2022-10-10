d8a5960776c59176d2dbdafaa8b2af6414a2800a
BRA <brand@boehme-weihs.de>
2022-10-11 01:31:24 +0200

    open new console or gui window

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,164 @@
 #include <iostream>
+#include <stdio.h>
+#include <io.h>
+#include <fcntl.h>
 #include <Windows.h>
-#include <tchar.h>
 #include "board.hpp"
 
-int main()
+#define CONSOLEBOARD
+
+#ifdef CONSOLEBOARD
+
+//===================TEST OPEN NEW CONSOLE==================
+int APIENTRY WinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR     lpCmdLine,
+                     int       nCmdShow)
 {
-    std::cout << "\nHello Chess Player \n\n";
+    AllocConsole();
 
-    Board chessBoard;
+    HANDLE handle_out = GetStdHandle(STD_OUTPUT_HANDLE);
+    int hCrt = _open_osfhandle((long) handle_out, _O_TEXT);
+    FILE* hf_out = _fdopen(hCrt, "w");
+    setvbuf(hf_out, NULL, _IONBF, 1);
+    *stdout = *hf_out;
 
-    //chessBoard.display();
+    HANDLE handle_in = GetStdHandle(STD_INPUT_HANDLE);
+    hCrt = _open_osfhandle((long) handle_in, _O_TEXT);
+    FILE* hf_in = _fdopen(hCrt, "r");
+    setvbuf(hf_in, NULL, _IONBF, 128);
+    *stdin = *hf_in;
 
-    char positions[32];
-    positions[0] = 0x44;
+    const char consoleTitle[6] = {'c','h','e','s','s','\0'};
+    LPCSTR pConsoleTitle = consoleTitle;
 
-    /*
-    Call c_str() to get a const char * (LPCSTR) from a std::string.
-
-    It's all in the name:
-
-    LPSTR - (long) pointer to string - char *
-
-    LPCSTR - (long) pointer to constant string - const char *
-
-    LPWSTR - (long) pointer to Unicode (wide) string - wchar_t *
-
-    LPCWSTR - (long) pointer to constant Unicode (wide) string - const wchar_t *
-
-    LPTSTR - (long) pointer to TCHAR (Unicode if UNICODE is defined, ANSI if not) string - TCHAR *
-
-    LPCTSTR - (long) pointer to constant TCHAR string - const TCHAR *
-
-    You can ignore the L (long) part of the names -- it's a holdover from 16-bit Windows.
-
-    */
-    const wchar_t myTw[3] = {'h','h','i'};    
-    LPCWSTR newTitleW( myTw );
-    SetConsoleTitle( newTitleW );
+    SetConsoleTitle( pConsoleTitle );
 
     HWND cmd = GetConsoleWindow();
     HDC hDC = GetDC(cmd);
 
-    SetPixel(hDC, 100, 100, RGB(200,200,200));
+
+    for( int r=100; r<200; r++)
+    {
+        for( int d=100; d<200; d++)
+        {
+            SetPixel(hDC, r, d, RGB(0xFF,0x00,0x00));
+        }
+    }
 
     ReleaseDC(cmd, hDC);
 	DeleteDC(hDC);
 
+    MSG msg;
+    while(GetMessage(& msg,NULL,0,0))
+    {
+        TranslateMessage(& msg);
+        DispatchMessage(& msg);
+    }
+    // use the console just like a normal one - printf(), getchar(), ...
+}
+
+#endif
+//==========================================================
+
+#ifdef GUIBOARD
+//===================TEST CODE DRAWING PIXEL================
+static HWND sHwnd;
+static COLORREF redColor=RGB(255,0,0);
+static COLORREF blueColor=RGB(0,0,255);
+static COLORREF greenColor=RGB(0,255,0);
+
+static COORD a = {150,150};
+static COORD b = {300,300};
+
+void SetWindowHandle(HWND hwnd)
+{
+    sHwnd=hwnd;
+}
+void setPixel(int x,int y,COLORREF& color=redColor)
+{
+    if(sHwnd==NULL)
+    {
+        MessageBox(NULL,"sHwnd was not initialized !","Error",MB_OK|MB_ICONERROR);
+        exit(0);
+    }
+    HDC hdc=GetDC(sHwnd);
+    SetPixel(hdc,x,y,color);
+    ReleaseDC(sHwnd,hdc);
+    return;
+}
+void drawStreightLine(COORD a, COORD b)
+{
+    for( int x = a.X; x < b.X; x++ )
+    {
+        setPixel(x, a.Y, greenColor);
+    }
+}
+LRESULT CALLBACK WndProc(HWND hwnd,UINT message,WPARAM wParam,LPARAM lParam)
+{
+    switch(message)
+    {
+    case WM_PAINT:
+        SetWindowHandle(hwnd);
+        drawStreightLine(a,b);
+        break;
+    case WM_CLOSE: // FAIL THROUGH to call DefWindowProc
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    default:
+        break; // FAIL to call DefWindowProc //
+    }
+    return DefWindowProc(hwnd,message,wParam,lParam);
+}
+int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,int iCmdShow)
+{
+    static TCHAR szAppName[] = TEXT("Test");
+    WNDCLASS wndclass;
+    wndclass.style         = CS_HREDRAW|CS_VREDRAW ;
+    wndclass.lpfnWndProc   = WndProc ;
+    wndclass.cbClsExtra    = 0 ;
+    wndclass.cbWndExtra    = 0 ;
+    wndclass.hInstance     = hInstance ;
+    wndclass.hIcon         = LoadIcon (NULL, IDI_APPLICATION) ;
+    wndclass.hCursor       = LoadCursor (NULL, IDC_ARROW) ;
+    wndclass.hbrBackground = (HBRUSH) GetStockObject (WHITE_BRUSH) ;
+    wndclass.lpszMenuName  = NULL ;
+    wndclass.lpszClassName = szAppName ;
+// Register the window //
+    if(!RegisterClass(& wndclass))
+    {
+        MessageBox(NULL,"Registering the class failled","Error",MB_OK|MB_ICONERROR);
+        exit(0);
+    }
+// CreateWindow //
+    HWND hwnd=CreateWindow(szAppName,"SetPixel example - programming-technique.blogspot.com",
+                           WS_OVERLAPPEDWINDOW,
+                           CW_USEDEFAULT,
+                           CW_USEDEFAULT,
+                           CW_USEDEFAULT,
+                           CW_USEDEFAULT,
+                           NULL,
+                           NULL,
+                           hInstance,
+                           NULL);
+    if(!hwnd)
+    {
+        MessageBox(NULL,"Window Creation Failed!","Error",MB_OK);
+        exit(0);
+    }
+// ShowWindow and UpdateWindow //
+    ShowWindow(hwnd,iCmdShow);
+    UpdateWindow(hwnd);
+// Message Loop //
+    MSG msg;
+    while(GetMessage(& msg,NULL,0,0))
+    {
+        TranslateMessage(& msg);
+        DispatchMessage(& msg);
+    }
     return 0;
 }
+
+#endif
